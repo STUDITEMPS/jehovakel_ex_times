21c2777c900d999b535ef0b143881727f8f746fc
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2025-03-13 09:50:35 +0000

    Bump version to 1.1.0
    We updated Timex which resulted in bugs being fixed that change
    behaviour and might impact users.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JehovakelExTimes.MixProject do
   def project do
     [
       app: :jehovakel_ex_times,
-      version: "1.0.4",
+      version: "1.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
